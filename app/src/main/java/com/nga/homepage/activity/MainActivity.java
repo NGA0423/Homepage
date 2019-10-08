f3c8b6e614868a0f11a845af1ff0024b7ea90205
ASUS <1834291158@qq.com>
2019-10-08 22:21:40 +0800

    第一次上传

---
+++ app/src/main/java/com/nga/homepage/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nga.homepage.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.nga.homepage.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
