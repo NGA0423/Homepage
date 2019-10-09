67ee9e6541204aae1be17b319bb5c59f27ff1bfc
ASUS <1834291158@qq.com>
2019-10-09 14:53:08 +0800

    引导页的实现

--- app/src/main/java/com/nga/homepage/activity/SplashActivity.java
+++ app/src/main/java/com/nga/homepage/activity/SplashActivity.java
@@ -1,6 +1,11 @@
 package com.nga.homepage.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +17,29 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
+
+        ImageView loadingView = findViewById(R.id.iv_loading);
+
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.splash_loading);
+        loadingView.startAnimation(animation);
+        animation.setFillAfter(true);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
