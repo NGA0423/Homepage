d8e2db1e1817ed0406f14c857c66fed62cdfcb06
huatec-1 <1834291158@qq.com>
2019-11-05 11:47:54 +0800

    登录

--- app/src/main/java/com/nga/homepage/activity/RegisterActivity.java
+++ app/src/main/java/com/nga/homepage/activity/RegisterActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.textclassifier.TextSelection;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
 import com.nga.homepage.R;
+import com.nga.homepage.bean.LoginResponse;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +19,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -34,7 +37,34 @@ public class RegisterActivity extends AppCompatActivity {
     }
     @OnClick(R.id.bt_longding)
     void login(){
+        String usersname = et_usersname.getText().toString();
+        String pwd = et_pwd.getText().toString();
+        String url="http://10.10.16.65:8089/MobileShop";
         //OkHttpClient build = new OkHttpClient.Builder().build();
+        OkHttpClient httpClient = new OkHttpClient.Builder().build();
+        FormBody body = new FormBody.Builder()
+                .add("input", usersname)
+                .add("password", pwd)
+                .build();
+        Request request = new Request.Builder().url(url).post(body).build();
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                Gson gson = new Gson();
+                LoginResponse loginResponse = gson.fromJson(json, LoginResponse.class);
+
+                String msg = loginResponse.getMsg();
+                String uname = loginResponse.getData().getUname();
+
+            }
+        });
+/*
         new Thread(){
             @Override
             public void run() {
@@ -42,9 +72,10 @@ public class RegisterActivity extends AppCompatActivity {
                 OkHttpClient httpClient=new OkHttpClient();
                 Request request = new Request
                         .Builder()
-                        .url("http://www.baidu.com")
+                        .url(url)
                         .get()
                         .build();
+*/
 /*
         httpClient.newCall(request).enqueue(new Callback() {
             @Override
@@ -57,14 +88,17 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
-*/
+*//*
+
                 try {
                     Response response = httpClient.newCall(request).execute();
+                    response.body().string();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }.start();
+*/
 
     }
 }
---
+++ app/src/main/java/com/nga/homepage/bean/LoginResponse.java
@@ -0,0 +1,33 @@
+package com.nga.homepage.bean;
+
+import android.provider.ContactsContract;
+
+public class LoginResponse {
+    private int status;
+    private String msg;
+    private MyData data;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public MyData getData() {
+        return data;
+    }
+
+    public void setData(MyData data) {
+        this.data = data;
+    }
+}
